2fce30df9000d201b29d5e94c2d624241d970c4d
Nick Anthony <nanthony007@gmail.com>
2021-06-15 16:32:19 -0400

    Add CORS support for pct-frontend.

--- app/main.py
+++ app/main.py
@@ -1,19 +1,33 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import markdown
-from fastapi import status
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import geocoding, pct_backend
 
 # TODO: add custom parameters
 # TODO: add subfolders with routers
 
-
 app = FastAPI(
     title="UK IPOP",
     description="This is a REST API for various endpoints provided by IPOP.",
     version="0.1.0",
 )
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://uk-ipop.github.io",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(geocoding.api)
 app.include_router(pct_backend.api)
